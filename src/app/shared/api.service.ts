672f0e2799a9c182962cba5b93557fc3fdda035c
Himanshu Sarroya <Sarroya06@gmail.com>
2022-06-03 17:10:45 +0530

    commit

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {} from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+}
